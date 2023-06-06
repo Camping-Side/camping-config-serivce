2824e5f87745dea737ce5428179a230ab2be39b2
dkask <dkaskgkdua@gmail.com>
2023-06-07 03:27:50 +0900

    feat: codedeploy 테스트

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-DEPLOY_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy.log"
+DEPLOY_LOG_PATH="/home/ubuntu/github_action/deploy.log"
 
-echo "===== 배포 시작 : $(date +%c) =====" >> $DEPLOY_LOG_PATH
+echo "===== 배포r 시작 : $(date +%c) =====" >> $DEPLOY_LOG_PATH
