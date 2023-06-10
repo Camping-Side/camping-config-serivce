05149f9245eafe19cb0f7bb7c2221db67bef5dfd
dkask <dkaskgkdua@gmail.com>
2023-06-11 01:12:35 +0900

    feat: logback 설정 추가

--- src/main/java/com/camping/configservice/controller/HeathController.java
+++ src/main/java/com/camping/configservice/controller/HeathController.java
@@ -1,12 +1,15 @@
 package com.camping.configservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HeathController {
     @GetMapping("/health-check")
     public String healthCheck() {
+        log.info("Config Server health-check");
         return "Config Server is Ok";
     }
 
