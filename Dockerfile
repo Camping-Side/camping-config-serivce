19d3ef290607fcf3dfa2924f04762451b0f48e66
dkask <dkaskgkdua@gmail.com>
2023-06-11 16:16:52 +0900

    feat: docker-compose 적용 테스트

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ RUN ["mkdir", "/var/log/config-service"]
 RUN ["chmod", "755", "/var/log/config-service"]
 VOLUME /data1/config-service /var/log/config-service
 COPY target/config-service-1.0.jar ConfigServer.jar
-ENTRYPOINT ["java", "-jar", "ConfigServer.jar"]
+ENTRYPOINT ["java", "-Dspring.profiles.active=dev", "-jar", "ConfigServer.jar"]
--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -14,12 +14,14 @@ EXIST_BLUE=$(docker ps -f "name=$SERVICE_NAME-blue" | grep Up)
 # docker run -d -p 9888:9888 -e "spring.profiles.active=dev" --name camping-config-service-blue ghcr.io/camping-side/camping-config-service:latest
 if [ -z "$EXIST_BLUE" ]; then
   echo "===== Blue Run Start =====" >> $DEPLOY_LOG_PATH
-  docker run -d -p 9888:9888 -e "spring.profiles.active=$PROFILE" --name $SERVICE_NAME-blue $DOCKER_IMAGE:$IMAGE_VERSION
+  docker-compose -p $SERVICE_NAME-blue -f docker-compose.blue.yml up -d
+#  docker run -d -p 9888:9888 -e "spring.profiles.active=$PROFILE" --name $SERVICE_NAME-blue $DOCKER_IMAGE:$IMAGE_VERSION
   STOP_TARGET_COLOR="green"
   START_TARGET_COLOR="blue"
 else
   echo "===== Green Run Start =====" >> $DEPLOY_LOG_PATH
-  docker run -d -p 9889:9888 -e "spring.profiles.active=$PROFILE" --name $SERVICE_NAME-green $DOCKER_IMAGE:$IMAGE_VERSION
+  docker-compose -p $SERVICE_NAME-green -f docker-compose.green.yml up -d
+#  docker run -d -p 9889:9888 -e "spring.profiles.active=$PROFILE" --name $SERVICE_NAME-green $DOCKER_IMAGE:$IMAGE_VERSION
   STOP_TARGET_COLOR="blue"
   START_TARGET_COLOR="green"
 fi
@@ -32,18 +34,19 @@ sleep 30
 START_SUCCESS=$(docker ps -f "name=$SERVICE_NAME-$START_TARGET_COLOR" | grep Up)
 # -n : 길이가 0이 아닌경우
 if [ -n "$START_SUCCESS" ]; then
-  docker stop $SERVICE_NAME-$STOP_TARGET_COLOR
-  sleep 3
-  docker rm $SERVICE_NAME-$STOP_TARGET_COLOR
+  docker-compose -p $SERVICE_NAME-$STOP_TARGET_COLOR -f docker-compose.$STOP_TARGET_COLOR.yml down
+#  docker stop $SERVICE_NAME-$STOP_TARGET_COLOR
+#  sleep 3
+#  docker rm $SERVICE_NAME-$STOP_TARGET_COLOR
   echo "===== 이전 컨테이너 종료 ====="  >> $DEPLOY_LOG_PATH
 fi
 
 # 신규 도커 이미지 삭제(다음 이미지 새로고침 위해)
-sleep 2
-docker rmi $DOCKER_IMAGE
+#sleep 2
+#docker rmi $DOCKER_IMAGE
 # none 이미지 삭제
-docker rmi $(docker images -f "dangling=true" -q)
-echo "===== 이미지 삭제 ====="  >> $DEPLOY_LOG_PATH
+#docker rmi $(docker images -f "dangling=true" -q)
+#echo "===== 이미지 삭제 ====="  >> $DEPLOY_LOG_PATH
 
 echo "===== $SERVICE_NAME 배포 종료 : $(date +%c) =====" >> $DEPLOY_LOG_PATH
 
